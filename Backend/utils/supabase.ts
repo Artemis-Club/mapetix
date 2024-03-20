17d3acc586254ec6860596d9e54d6dc0778f5523
Xadow0 <leoyotrosanimales@gmail.com>
2024-03-20 23:14:01 +0100

    Cambio de proyecto de Supabase al que vamos a utilizar para gestionar la bd

--- Backend/utils/supabase.ts
+++ Backend/utils/supabase.ts
@@ -2,8 +2,8 @@ import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://xvmcmtlooajfhaowlcjc.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2bWNtdGxvb2FqZmhhb3dsY2pjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA0NDAxMjQsImV4cCI6MjAyNjAxNjEyNH0.Ry53_urbPY13AKtiwejVwJYHsxZn8P42xwJTnK6PDrY";
+const supabaseUrl = "https://kuutjjaknhwnlijjacqt.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1dXRqamFrbmh3bmxpamphY3F0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAxNDU5ODUsImV4cCI6MjAyNTcyMTk4NX0.ohRmkp2VHIx5SuaLadNyyDOs9xlqzwRw7pfv_cSVUKE";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
