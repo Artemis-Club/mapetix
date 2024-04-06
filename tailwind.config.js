4cb71ed38ce8d9ca076f1f3cbab53b2e14cb19d1
Pedro Gardim <me@pedrogardim.com>
2024-04-06 12:34:40 +0200

    add kumbh font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      thin: ["KumbhSans_100Thin", "sans-serif"],
+      regular: ["KumbhSans_400Regular", "sans-serif"],
+      bold: ["KumbhSans_800ExtraBold", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
