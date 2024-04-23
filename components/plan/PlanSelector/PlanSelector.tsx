64976377ec9617ee05092aa56d788ec5c3ff27ed
Pedro Gardim <me@pedrogardim.com>
2024-04-24 00:59:14 +0200

    add planselector component

---
+++ components/plan/PlanSelector/PlanSelector.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import Text from '../../ui/atoms/Text/Text';
+
+import { FlatList, TouchableOpacity, View } from 'react-native';
+import clsx from 'clsx';
+import { useRouter } from 'expo-router';
+import { useGetMyPlansQuery } from '@/api/plan';
+
+interface PlanSelectorProps {
+  onPlanSelected?: (planId: string) => void;
+  className?: string;
+}
+
+const PlanSelector: React.FC<PlanSelectorProps> = ({
+  onPlanSelected,
+  ...props
+}) => {
+  const { data: plans, error, isLoading } = useGetMyPlansQuery();
+
+  const renderItem = ({
+    item: { description, start_date, total_price, plan_id },
+  }: any) => (
+    <TouchableOpacity
+      onPress={() => onPlanSelected && onPlanSelected(plan_id)}
+      key={plan_id}
+    >
+      <View
+        className={clsx(
+          'border-b border-neutral-600 py-2 px-4',
+          'flex-row justify-between'
+        )}
+      >
+        <View className="flex-col">
+          <Text className="text-white text-base">{description}</Text>
+          <Text className="text-neutral-500 text-xs">{start_date}</Text>
+        </View>
+        <View className="flex-col justify-center">
+          <Text bold className="text-emerald-200">
+            €{total_price}
+          </Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+
+  return (
+    <FlatList
+      data={plans}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.plan_id}
+      {...props}
+    />
+  );
+};
+
+export default PlanSelector;
