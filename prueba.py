0c820e09acdafb05b82458d9db3fcbd8d2427f5c
NotJaviX <javierlloret2011@gmail.com>
2024-05-01 21:18:22 +0200

    .

--- prueba.py
+++ prueba.py
@@ -10,8 +10,11 @@ supabase_key = os.getenv('SUPABASE_KEY')
 supabase = create_client(supabase_url, supabase_key)
 
 supabase.auth.sign_in_with_password({"email": "carlos@mail.com", "password": "Rest1234_"})
-
+res = supabase.auth.get_session()
 data = supabase.auth.get_user()
+print('El user : ')
 print(data)
+print('La sesion :')
+print(res)
 res2 = supabase.auth.sign_out()
 
