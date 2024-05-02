72bd66ab47bdc3f472a4d716778af34222992934
NotJaviX <javierlloret2011@gmail.com>
2024-05-02 12:37:43 +0200

    lista de users,get access_token(jwt) y user a partir de jwt

--- prueba.py
+++ prueba.py
@@ -4,17 +4,27 @@ import os
 from dotenv import load_dotenv
 
 
+
 load_dotenv()
 supabase_url = os.getenv('SUPABASE_URL')
-supabase_key = os.getenv('SUPABASE_KEY')
+supabase_key = os.getenv('SUPABASE_KEY2')
 supabase = create_client(supabase_url, supabase_key)
 
 supabase.auth.sign_in_with_password({"email": "carlos@mail.com", "password": "Rest1234_"})
-res = supabase.auth.get_session()
-data = supabase.auth.get_user()
-print('El user : ')
-print(data)
-print('La sesion :')
-print(res)
+#users = supabase.auth.admin.list_users()
+session = supabase.auth.get_session().access_token
+print('sesion:')
+print(session)
+userjwt = supabase.auth.get_user(session)
+print('userjwt:')
+print(userjwt)
+#res = supabase.auth.get_session()
+#data = supabase.auth.get_user()
+#print('Todos los users :')
+#print(users)
+#print('El user : ')
+#print(data)
+#print('La sesion :')
+#print(res)
 res2 = supabase.auth.sign_out()
 
